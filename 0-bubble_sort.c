ab5f8aab9d7895ea2b350df10bda089a7929b98c
Genzord111 <feliciensolomon5@gmail.com>
2024-10-26 14:28:03 -0700

    reduced function

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,35 +2,35 @@
 #include <stdbool.h>
 #include "sort.h"
 
-/*
+/**
  * bubble_sort - sorts an array using bubble sort algorithm
  * @array: Pointer to array
  * @size: size of array
  * Return: void
- */
+*/
 
 void bubble_sort(int *array, size_t size)
 {
-    size_t i;
-    int temp;
-    bool swapped = true;
+	size_t current;
+	int temp;
+	bool swapped = true;
 
-    if (size < 2)
-        return;
+	if (size < 2)
+		return;
 
-    while (swapped)
-    {
-        swapped = false;
-        for (i = 0; i < size - 1; i++)
-        {
-            if (array[i] > array[i + 1])
-            {
-                temp = array[i];
-                array[i] = array[i + 1];
-                array[i + 1] = temp;
-                print_array(array, size);
-                swapped = true;
-            }
-        }
-    }
+	while (swapped)
+	{
+		swapped = false;
+		for (current = 0; current < size - 1; current++)
+		{
+			if (array[current] > array[current + 1])
+			{
+				temp = array[current];
+				array[current] = array[current + 1];
+				array[current + 1] = temp;
+				print_array(array, size);
+				swapped = true;
+			}
+		}
+	}
 }
