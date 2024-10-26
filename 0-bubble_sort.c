495e7eac1880c437fe51c249d6d1feacc9d3e5f1
Genzord111 <feliciensolomon5@gmail.com>
2024-10-26 12:53:12 -0700

    edit to 0-bubble_sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,56 +2,63 @@
 #include <stdbool.h>
 #include "sort.h"
 
+/*
+ * bubble_sort - sorts an array using bubble sort algorithm
+ * @array: Pointer to array
+ * @size: size of array
+ * Return: void
+ */
+
 void bubble_sort(int *array, size_t size)
 {
-  int current_index = 0;
-    int next_index;
-    int temp;
-    int indexTracked = 0;
-    bool isSorted = false;
-    bool isSwappable;
-    bool beingTracked;
-    int count = 0;
+	unsigned long int current_index = 0;
+	unsigned long int next_index;
+	int temp;
+	int indexTracked = 0;
+	bool isSorted = false;
+	bool isSwappable;
+	bool beingTracked;
+	int count = 0;
 
-    if (size < 2)
-        return;
+	if (size < 2)
+		return;
 
-        while(isSorted == false)
-        {
-            count++;
-            isSwappable = false;
-            beingTracked = false;
+	while (isSorted == false)
+	{
+		count++;
+		isSwappable = false;
+		beingTracked = false;
 
-            for (current_index = 0; current_index < size - 1; current_index++)
-            {
-                next_index = current_index + 1;
-                if (array[current_index] > array[next_index])
-                    {
-                        indexTracked = current_index;
-                        isSwappable = true;
-                        beingTracked = true;
-                        break;
-                    }
-            }
-          
-        while (beingTracked == true)
-        {
-          next_index = indexTracked + 1;
-          if (next_index < size && array[indexTracked] > array[next_index])
-          {
-            temp = array[indexTracked];    
-            array[indexTracked] = array[next_index];
-            array[next_index] = temp;
-            print_array(array, size);
-            indexTracked++;
-          }
-          else
-            beingTracked = false;
-        }
+		for (current_index = 0; current_index < size - 1; current_index++)
+		{
+			next_index = current_index + 1;
+			if (array[current_index] > array[next_index])
+			{
+				indexTracked = current_index;
+				isSwappable = true;
+				beingTracked = true;
+				break;
+			}
+		}
 
-        if (isSwappable == false)
-        {
-          isSorted = true;
-        }
-      }
+		while (beingTracked == true)
+		{
+			next_index = indexTracked + 1;
+			if (next_index < size && array[indexTracked] > array[next_index])
+			{
+				temp = array[indexTracked];
+				array[indexTracked] = array[next_index];
+				array[next_index] = temp;
+				print_array(array, size);
+				indexTracked++;
+			}
+			else
+				beingTracked = false;
+		}
+
+		if (isSwappable == false)
+		{
+			isSorted = true;
+		}
+	}
 }
